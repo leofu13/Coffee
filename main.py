b1ad8c256270a81b8a4ed36e16913fd5a0282d57
Leo Fu <leofu13@gmail.com>
2025-10-27 16:06:20 -0700

    Added user brewing ratio preference

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #Prompt User to input amount of coffee grounds
 Coffee_str = input("Enter Amount of coffee grounds in grams: ")
 Coffee_int = int(Coffee_str)
-
 x = int(Coffee_str)
 
-#Outputs amount of water needed in grams
-print(x * 16,"grams of water")
+#Prompts User for Coffee to Water Ratio Preference and provides output
+print("Enter desired coffee to water ratio: 1/16 or 1/17")
+y = input()
+
+if y == "1/16":
+
+    print(x * 16,"grams of water")
+
+else:
+
+    print(x * 17,"grams of water")
+
