990afb22b157a4383ddad852f7a27f95b432310f
Leo Fu <leofu13@gmail.com>
2025-10-27 15:31:48 -0700

    Added comments

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#Prompt User to input amount of coffee grounds
 Coffee_str = input("Enter Amount of coffee grounds in grams: ")
 Coffee_int = int(Coffee_str)
 
 x = int(Coffee_str)
 
+#Outputs amount of water needed in grams
 print(x * 16,"grams of water")
