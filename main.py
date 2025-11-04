f7bd922195f8e07aaa48234b9458aa625f365f61
Leo Fu <leofu13@gmail.com>
2025-11-04 15:57:54 -0800

    Added numerical input validation

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-#Prompt User to input amount of coffee grounds
-Coffee_str = input("Enter Amount of coffee grounds in grams: ")
-Coffee_int = int(Coffee_str)
-x = int(Coffee_str)
+#Input Validation
+def inputNumber(message):
+    while True:
+        try:
+            userInput = int(input(message))
+        except ValueError:
+            print("Please input a numerical value.")
+            continue
+        else:
+            return userInput
+            break
+#Prompt user to input the amount of coffee grounds
+Coffee_str = inputNumber("Please input amount of coffee grounds in grams: ")
+x = Coffee_str
 
 #Prompts User for Coffee to Water Ratio Preference and provides output
-print("Enter desired coffee to water ratio: 1/16 or 1/17")
-y = input()
+Brew_ratio = input("Enter desired coffee to water ratio: 1:16 or 1:17: ")
+y = Brew_ratio
 
-if y == "1/16":
+if y == "1:16":
 
     print(x * 16,"grams of water")
 
